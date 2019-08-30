56ec06c71390c82e3b349b92edb1beeaada1d5f1
Steve Megson <steve@honestjohn.co.uk>
2019-08-30 14:37:34 +0100

    Multiple user picker

--- src/Umbraco.Core/Constants-PropertyEditors.cs
+++ src/Umbraco.Core/Constants-PropertyEditors.cs
@@ -172,6 +172,11 @@ namespace Umbraco.Core
                 public const string UserPicker = "Umbraco.UserPicker";
 
                 /// <summary>
+                /// Multiple User Picker.
+                /// </summary>
+                public const string MultipleUserPicker = "Umbraco.MultipleUserPicker";
+
+                /// <summary>
                 /// Upload Field.
                 /// </summary>
                 public const string UploadField = "Umbraco.UploadField";
